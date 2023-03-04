8b4e96d2ded4fd67e24e030c8bc99ac41e14482d
HuangJiaChen1 <jasonhuanggggg@gmail.com>
2023-03-04 13:52:30 -0700

    done

--- app/src/main/java/com/example/myapplication/ProfileActivity.java
+++ app/src/main/java/com/example/myapplication/ProfileActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +12,14 @@ public class ProfileActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
+        Button backButton = findViewById(R.id.back_button3);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileActivity.this, InMyAccountActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
