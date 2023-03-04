911df4e99106057934ca9b208a985f72371c573e
vetarnb <xinhao@ualberta.ca>
2023-03-04 14:46:07 -0700

    Xinhao

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,117 @@
+package com.example.myapplication;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class    MainActivity extends AppCompatActivity {
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button recordButton = findViewById(R.id.Scan);
+        recordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Show a dialog asking the user if they want to record the location
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("Do you want to record the location?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // Show another dialog asking if the user wants to record an image
+                                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                                builder.setMessage("Do you want to record an image?")
+                                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int id) {
+                                                // Show a dialog allowing the user to choose between taking a photo or uploading from the gallery
+                                                final CharSequence[] options = {"Take Photo", "Choose from Gallery"};
+                                                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                                                builder.setTitle("Choose an option");
+                                                builder.setItems(options, new DialogInterface.OnClickListener() {
+                                                    @Override
+                                                    public void onClick(DialogInterface dialog, int which) {
+                                                        if (options[which].equals("Take Photo")) {
+
+                                                        } else if (options[which].equals("Choose from Gallery")) {
+
+                                                        }
+                                                    }
+                                                });
+                                                builder.show();
+                                            }
+                                        })
+                                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int id) {
+                                                // Show a dialog allowing the user to input a comment
+                                                // TODO: Add code to allow comment input
+                                            }
+                                        });
+                                builder.create().show();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                                builder.setMessage("Do you want to record an image?")
+                                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int id) {
+                                                // Show a dialog allowing the user to upload an image
+                                                // TODO: Add code to allow image upload
+                                            }
+                                        })
+                                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int id) {
+                                                // Show a dialog allowing the user to input a comment
+                                                // TODO: Add code to allow comment input
+                                            }
+                                        });
+                                builder.create().show();
+                            }
+                        });
+                builder.create().show();
+            }
+
+        });
+
+
+
+
+
+
+
+
+
+
+//RANKING_BUTTON
+        Button rankButton = findViewById(R.id.rankButton);
+        rankButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an AlertDialog to display the rank image
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Rank Image");
+                builder.setMessage("This is your rank image.");
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Close the dialog
+                        dialog.dismiss();
+                    }
+                });
+                builder.show();
+            }
+        });
+    }
+}
