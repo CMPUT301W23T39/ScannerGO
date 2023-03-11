d66f93f835c198e072728fe9a0859fb73635a3f9
vetarnb <xinhao@ualberta.ca>
2023-03-11 15:55:01 -0700

    update rank

--- app/src/main/java/com/example/myapplication/FireBaseRankActivity.java
+++ app/src/main/java/com/example/myapplication/FireBaseRankActivity.java
@@ -7,29 +7,19 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class InMyRankActivity extends AppCompatActivity {
+public class FireBaseRankActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.rank);
+        setContentView(R.layout.firebase_rank);
         Button backButton = findViewById(R.id.back_button);
-        Button FirebaseButton = findViewById(R.id.firebase_button);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(InMyRankActivity.this,MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-        FirebaseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(InMyRankActivity.this,FireBaseRankActivity.class);
+                Intent intent = new Intent(FireBaseRankActivity.this, InMyRankActivity.class);
                 startActivity(intent);
                 finish();
             }
         });
     }
-
 }
