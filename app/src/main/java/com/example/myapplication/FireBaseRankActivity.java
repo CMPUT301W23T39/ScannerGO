4955c159734991470cf99f355b240c239d7e2855
HuangJiaChen1 <jasonhuanggggg@gmail.com>
2023-03-12 15:40:12 -0600

    debugged

--- app/src/main/java/com/example/myapplication/FireBaseRankActivity.java
+++ app/src/main/java/com/example/myapplication/FireBaseRankActivity.java
@@ -51,7 +51,7 @@ public class FireBaseRankActivity extends AppCompatActivity {
         CollectionReference userCollection = db.collection("username");
 
         DocumentReference userDocRef = userCollection.document(currUsername);
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("myData");
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("QR Codes");
         CollectionReference qrCodesCollection = userDocRef.collection("QR Codes");
 
 
@@ -115,38 +115,29 @@ public class FireBaseRankActivity extends AppCompatActivity {
                     // Retrieve the "Name" field of each document in the subcollection
 
                     String name = document.getString("Name");
-                    String visual = document.getString("Visual");
-//                    StringBuilder faceBuilder = new StringBuilder();
-//                    char hands = visual.charAt(4);
-//                    char ears = visual.charAt(1);
-//                    char eyes = visual.charAt(0);
-//                    char eyebrows = visual.charAt(2);
-//                    char mouth = visual.charAt(3);
-//                    char flower = visual.charAt(5);
-//                    faceBuilder.append(hands).append(ears).append('(').append(eyebrows).append(eyes).append(mouth).append(eyes).append(eyebrows).append(')').append(flower).append(ears).append(hands);
-//                    String face = faceBuilder.toString();
-                    qrCodesList.add(name+"\n");
+                    qrCodesList.add(name + "\n");
 
                 }
-                adapter = new ArrayAdapter<>(FireBaseRankActivity.this, android.R.layout.simple_list_item_1,qrCodesList);
+                adapter = new ArrayAdapter<>(FireBaseRankActivity.this, android.R.layout.simple_list_item_1, qrCodesList);
                 rankList.setAdapter(adapter);
                 // Do something with the ArrayList of names
                 System.out.println(qrCodesList);
 
                 for (QueryDocumentSnapshot document : task.getResult()) {
-                    // Retrieve the "Score" field of each document in the subcollection
-                    Integer score = document.getLong("Score").intValue();
+                    Integer score = document.getLong("Score") != null ? document.getLong("Score").intValue() : 0;
                     scoresList.add(score);
                 }
+
                 // Use built-in Java methods to find the lowest and highest score
-                int lowestScore = Collections.min(scoresList);
-                int highestScore = Collections.max(scoresList);
+                int lowestScore = 0;
+                int highestScore = 0;
+                if (!scoresList.isEmpty()) {
+                    lowestScore = Collections.min(scoresList);
+                    highestScore = Collections.max(scoresList);
+                }
                 // Do something with the lowest and highest score
                 System.out.println("Lowest score: " + lowestScore);
                 System.out.println("Highest score: " + highestScore);
-
-
-
 
 
             } else {
@@ -185,95 +176,3 @@ public class FireBaseRankActivity extends AppCompatActivity {
     }
 
 }
-
-//package com.example.myapplication;
-//
-//import android.content.Intent;
-//import android.os.Bundle;
-//import android.view.View;
-//import android.widget.ArrayAdapter;
-//import android.widget.Button;
-//import android.widget.ListView;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//import com.google.firebase.FirebaseApp;
-//import com.google.firebase.firestore.CollectionReference;
-//import com.google.firebase.firestore.DocumentReference;
-//import com.google.firebase.firestore.FirebaseFirestore;
-//
-//import java.util.ArrayList;
-//
-//public class FireBaseRankActivity extends AppCompatActivity {
-//    private ArrayAdapter<String> adapter;
-//    public String currUsername = "1234";
-//
-//
-//    protected void onCreate(Bundle savedInstanceState) {
-//
-//
-//        ListView qrList = findViewById(R.id.qr_list);
-//        FirebaseApp.initializeApp(this);
-//        ArrayList<String> qrCodesList = new ArrayList<>();
-//
-//        FirebaseFirestore DB = FirebaseFirestore.getInstance();
-//
-//        CollectionReference userCollection = DB.collection("username");
-//        DocumentReference userDocRef = userCollection.document("Ranking");
-//
-//        CollectionReference qrCodesCollection = userDocRef.collection("QR Codes");
-//
-//        userDocRef.get(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                ArrayList<String> rankingsList = new ArrayList<>();
-//                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-//                    String ranking = snapshot.getValue(String.class);
-//                    rankingsList.add(ranking);
-//                }
-//                ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, rankingsList);
-//                listView.setAdapter(adapter);
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//                Log.d("TAG", "Failed to read value.", databaseError.toException());
-//            }
-//        });
-//
-//
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.firebase_rank);
-//        Button backButton = findViewById(R.id.back_button);
-//        backButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(FireBaseRankActivity.this, InMyRankActivity.class);
-//                startActivity(intent);
-//                finish();
-//            }
-//        });
-//    }
-////    FirebaseDatabase DB = FirebaseDatabase.getInstance();
-////    DatabaseReference ref = database.getReference("rankings");
-////
-////    ListView listView = findViewById(R.id.listView);
-////
-////    ref.addValueEventListener(new ValueEventListener() {
-////        @Override
-////        public void onDataChange(DataSnapshot dataSnapshot) {
-////            ArrayList<String> rankingsList = new ArrayList<>();
-////            for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-////                String ranking = snapshot.getValue(String.class);
-////                rankingsList.add(ranking);
-////            }
-////            ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, rankingsList);
-////            listView.setAdapter(adapter);
-////        }
-////
-////        @Override
-////        public void onCancelled(DatabaseError databaseError) {
-////            Log.d("TAG", "Failed to read value.", databaseError.toException());
-////        }
-////    });
-//}
