67f56168f1aaf99b98101e436210cdec0758d13f
Mingze2379 <122427329+Mingze2379@users.noreply.github.com>
2023-03-11 23:34:08 -0700

    Add back button

--- app/src/main/java/com/example/myapplication/map.java
+++ app/src/main/java/com/example/myapplication/map.java
@@ -6,6 +6,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import android.content.Intent;
 import android.location.Location;
 import android.Manifest;
 
@@ -20,6 +21,8 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -45,11 +48,13 @@ public class map extends AppCompatActivity {
     SupportMapFragment supportMapFragment;
     FusedLocationProviderClient client;
     GoogleMap googleMap;
+    private Button back;
     ArrayList<LatLng> latLngList = new ArrayList<>();  // Declare latLngList as a member variable
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map);
+        back = findViewById(R.id.backFromMap);
         FirebaseApp.initializeApp(this);
         getUserLocation();
         supportMapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -60,6 +65,14 @@ public class map extends AppCompatActivity {
         else {
             ActivityCompat.requestPermissions(map.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
         }
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(map.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
     // Add this method to add markers to the map
