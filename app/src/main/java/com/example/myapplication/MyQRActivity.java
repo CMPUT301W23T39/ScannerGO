13cc78bced5c7cdfa6b34bec907bcc937afe381e
HuangJiaChen1 <jasonhuanggggg@gmail.com>
2023-04-03 20:05:34 -0600

    update qr list

--- app/src/main/java/com/example/myapplication/MyQRActivity.java
+++ app/src/main/java/com/example/myapplication/MyQRActivity.java
@@ -8,13 +8,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.ImageView;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.ListResult;
-import com.google.firebase.storage.StorageReference;
-import com.squareup.picasso.Picasso;
-
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -26,36 +21,32 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.GeoPoint;
-import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.type.LatLng;
-
-import org.w3c.dom.Text;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.ListResult;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
 public class MyQRActivity extends AppCompatActivity {
-    String sameu = "Usernames with same QR Code: ";
     String Hash;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.myqr);
+        setContentView(R.layout.other_qr);
         TextView QRCodeName = findViewById(R.id.qrcode_name);
 
         TextView CommentText = findViewById(R.id.comment_text);
         TextView LocationText = findViewById(R.id.location_text);
         TextView ScoreText = findViewById(R.id.score);
-        TextView SameUsersText = findViewById(R.id.sameUsers);
         Button backButton = findViewById(R.id.back_button2);
-        Button deleteButton = findViewById(R.id.delete_button);
         Intent intent = getIntent();
         String QRCode = intent.getStringExtra("QRCode");
-        Hash = intent.getStringExtra("Hash");
         QRCodeName.setText("Name: "+QRCode);
 
         ImageView userImage = findViewById(R.id.loc_image);
-        String username = loginActivity.username1;
+        String username = intent.getStringExtra("username");
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         CollectionReference userCollection = db.collection("username");
         DocumentReference userDocRef = userCollection.document(username);
@@ -65,79 +56,32 @@ public class MyQRActivity extends AppCompatActivity {
 // Get the document with ID "some username" from the "username" collection
 
         qrCodesCollection.get().addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        for (QueryDocumentSnapshot document : task.getResult()) {
-                            if (QRCode.equals(document.getString("Name"))) {
-                                String comment = document.getString("Comment");
-                                CommentText.setText("Comment: " + comment);
-                                Long score = document.getLong("Point");
-                                ScoreText.setText("Score: " + score);
-                                GeoPoint location = document.getGeoPoint("Location");
-                                double lat = location.getLatitude();
-                                double lng = location.getLongitude();
-                                String loc = lat + ", " + lng;
-                                LocationText.setText("Location: " + loc);
-                                Hash = document.getString("HASH");
-                                loadImage(username,userImage);
-                            }
-                        }
-                    }
-                });
-        CollectionReference qrCollection = db.collection("QR Codes");
-        DocumentReference qrDocRef = qrCollection.document(Hash);
-        CollectionReference SameUserCollection = qrDocRef.collection("users");
-
-        SameUserCollection.get().addOnCompleteListener(task -> {
             if (task.isSuccessful()) {
                 for (QueryDocumentSnapshot document : task.getResult()) {
-
-                    if (!username.equals(document.getString("ID"))) {
-                        sameu += "\n"+document.getString("ID");
-                        SameUsersText.setText(sameu);
+                    if (QRCode.equals(document.getString("Name"))) {
+                        String comment = document.getString("Comment");
+                        CommentText.setText("Comment: " + comment);
+                        Long score = document.getLong("Point");
+                        ScoreText.setText("Score: " + score);
+                        GeoPoint location = document.getGeoPoint("Location");
+                        double lat = location.getLatitude();
+                        double lng = location.getLongitude();
+                        String loc = lat + ", " + lng;
+                        LocationText.setText("Location: " + loc);
+                        Hash = document.getString("HASH");
+                        loadImage(username,userImage);
                     }
-                }}});
-
+                }
+            }
+        });
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MyQRActivity.this, FireBaseRankActivity.class);
+                Intent intent = new Intent(MyQRActivity.this, QRListActivity.class);
+                intent.putExtra("username", username);
                 startActivity(intent);
                 finish();
-            }
-        });
-        deleteButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                qrCodesCollection.get().addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        for (QueryDocumentSnapshot document : task.getResult()) {
-                            String name = document.getString("Name");
-                            if (name.equals(QRCode)) {
-                                document.getReference().delete().addOnSuccessListener(new OnSuccessListener<Void>() {
-                                    @Override
-                                    public void onSuccess(Void aVoid) {
-                                        Log.d(TAG, "DocumentSnapshot successfully deleted!");
-
-
-                                        Intent intent = new Intent(MyQRActivity.this, FireBaseRankActivity.class);
-
-                                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                                        startActivity(intent);
-                                        finish();
-                                    }
-                                }).addOnFailureListener(new OnFailureListener() {
-                                    @Override
-                                    public void onFailure(@NonNull Exception e) {
-                                        Log.w(TAG, "Error deleting document", e);
-                                    }
-                                });
-                            }
-                        }
-                    } else {
-                        System.out.println("Error getting documents: " + task.getException());
-                    }
-                });
             }
         });
 
@@ -189,5 +133,4 @@ public class MyQRActivity extends AppCompatActivity {
         }
         return lastJpgRef;
     }
-
 }
--- app/src/main/java/com/example/myapplication/QRListActivity.java
+++ app/src/main/java/com/example/myapplication/QRListActivity.java
@@ -10,6 +10,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,6 +19,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -25,6 +31,7 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class QRListActivity extends AppCompatActivity {
@@ -36,18 +43,58 @@ public class QRListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.qr_list);
-        ListView qrList = findViewById(R.id.qr_list);
-        Button backButton = findViewById(R.id.back_button4);
+        setContentView(R.layout.firebase_rank);
+
+        Intent intent = getIntent();
+        String currUsername = loginActivity.username1;
+        ListView rankList = findViewById(R.id.rank_list);
+        Button backButton = findViewById(R.id.back_button);
+        TextView highlowCode = findViewById(R.id.highlow_code);
         FirebaseApp.initializeApp(this);
         ArrayList<String> qrCodesList = new ArrayList<>();
+        ArrayList<Integer> scoresList = new ArrayList<>();
+
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-
         CollectionReference userCollection = db.collection("username");
-        DocumentReference userDocRef = userCollection.document(currUsername);
 
+        DocumentReference userDocRef = userCollection.document(currUsername);
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("QR Codes");
         CollectionReference qrCodesCollection = userDocRef.collection("QR Codes");
+
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                // Do something with the value
+
+                // Find the lowest and highest score
+                long lowestScore = Long.MAX_VALUE;
+                long highestScore = Long.MIN_VALUE;
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Long score = snapshot.child("Score").getValue(Long.class);
+                    if (score != null) {
+                        if (score < lowestScore) {
+                            lowestScore = score;
+                        }
+                        if (score > highestScore) {
+                            highestScore = score;
+                        }
+                    }
+                }
+
+                // Do something with the lowest and highest score
+                System.out.println("Lowest score: " + lowestScore);
+                System.out.println("Highest score: " + highestScore);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Handle errors
+            }
+        });
+
 
         qrCodesCollection.get().addOnCompleteListener(task -> {
             if (task.isSuccessful()) {
@@ -64,46 +111,62 @@ public class QRListActivity extends AppCompatActivity {
                     char flower = visual.charAt(5);
                     faceBuilder.append(hands).append(ears).append('(').append(eyebrows).append(eyes).append(mouth).append(eyes).append(eyebrows).append(')').append(flower).append(ears).append(hands);
                     String face = faceBuilder.toString();
-                    qrCodesList.add(name+"\n"+face);
+                    qrCodesList.add(name + "\n" + face);
 
                 }
-                adapter = new ArrayAdapter<>(QRListActivity.this, android.R.layout.simple_list_item_1,qrCodesList);
-                qrList.setAdapter(adapter);
+                adapter = new ArrayAdapter<>(QRListActivity.this, android.R.layout.simple_list_item_1, qrCodesList);
+                rankList.setAdapter(adapter);
                 // Do something with the ArrayList of names
                 System.out.println(qrCodesList);
+
+                for (QueryDocumentSnapshot document : task.getResult()) {
+                    Integer score = document.getLong("Point") != null ? document.getLong("Point").intValue() : 0;
+                    scoresList.add(score);
+                }
+
+                // Use built-in Java methods to find the lowest and highest score
+                int lowestScore = 0;
+                int highestScore = 0;
+                if (!scoresList.isEmpty()) {
+                    lowestScore = Collections.min(scoresList);
+                    highestScore = Collections.max(scoresList);
+                }
+                int size = scoresList.size();
+                // Do something with the lowest and highest score
+                System.out.println("Lowest score: " + lowestScore);
+                System.out.println("Highest score: " + highestScore);
+                highlowCode.setText("Highest QRcode: " + highestScore + "            " + "Lowest QRcode:" + lowestScore
+                        + "\n" + "Total: " + size);
+
             } else {
                 System.out.println("Error getting documents: " + task.getException());
             }
         });
 
 
-        qrList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        rankList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(QRListActivity.this, MyQRActivity.class);
                 String wholeQRCode = (String) parent.getItemAtPosition(position);
-                String QRCode = wholeQRCode.substring(0,wholeQRCode.indexOf("\n"));
+                String QRCode = wholeQRCode.substring(0, wholeQRCode.indexOf("\n"));
+                intent.putExtra("username",currUsername);
                 intent.putExtra("QRCode", QRCode);
                 startActivity(intent);
+                finish();
             }
         });
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(QRListActivity.this, InMyAccountActivity.class);
+                intent.putExtra("username",currUsername);
                 startActivity(intent);
+                finish();
             }
         });
 
 
     }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        // Notify the adapter that the data has changed only if it is not null
-        if (adapter != null) {
-            adapter.notifyDataSetChanged();
-        }
-    }
-
 }
+
